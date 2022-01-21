75eadb72fdbcef88c4b820207a53cb571fe628d6
OlgaTikhonova <Olga.Tikhonova@flexdatabases.com>
2022-01-21 20:04:32 +0300

    Задание 13

--- addressbook-web-tests/addressbook-web-tests/tests/tAddressBookAdressCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/tAddressBookAdressCreationTests.cs
@@ -7,12 +7,30 @@ namespace WebAddressbookTests
     [TestFixture]
     public class AddressBookAdressCreationTests : AuthTestBase
     {
-        [Test]
-        public void AddressCreationTest()
+        public static IEnumerable<AddressData> RandomAddressProvider()
         {
-            AddressData address = new AddressData("First Name " + DateTime.Now, "lastname " + DateTime.Now)
-            ;
-
+            List<AddressData> address = new List<AddressData>();
+            for (int i = 0; i < 5; i++)
+            {
+                address.Add(new AddressData(GenerateRandomString(10), GenerateRandomString(10))
+                {
+                    MiddleName = GenerateRandomString(10),
+                    Address = GenerateRandomString(10),
+                    Phone2 = GenerateRandomString(10),
+                    NickName = GenerateRandomString(10),
+                    Address2 = GenerateRandomString(10),
+                    Mail1 = GenerateRandomString(10),
+                    WorkPhone = GenerateRandomString(10),
+                    HomePhone = GenerateRandomString(10),
+                    Mail2 = GenerateRandomString(10),
+                    Mail3 = GenerateRandomString(10)
+                });
+            }
+            return address;
+        }
+        [Test, TestCaseSource("RandomAddressProvider")]
+        public void AddressCreationTest(AddressData address)
+        {
             List<AddressData> oldAddress = app.Address.GetAddressList();
             app.Address.CreateAddress(address);
             Assert.AreEqual(oldAddress.Count + 1, app.Address.GetAddressCount());
